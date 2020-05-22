64a54c27007154ca55e94321b05e9dd8483be5d7
aryehlev <59808808+aryehlev@users.noreply.github.com>
2020-05-22 15:39:20 +0300

    rak-gigi

--- Match_game _logic/Game.cs
+++ Match_game _logic/Game.cs
@@ -8,37 +8,54 @@ namespace Match_game__logic
 {
     class Game
     {
-        private char[,] m_LettersBehindCards;
-        private GameBoard m_GameBoard;
+        private Card[,] m_GameBoard;
         private string m_NameOfPlayer1;
         private string m_NameOfPlayer2;
         private bool m_MultiPlayerMode;
         private int m_Player1Score;
         private int m_Player2Score;
-
+        private Card m_
         public Game(int i_NumberOfRows, int i_NumberOfColumns, string i_NameOfPlayer1, bool i_MultiPlayerMode, string i_NameOfPlayer2 = "Computer")
         {
-            this.m_GameBoard = new GameBoard(i_NumberOfRows, i_NumberOfColumns);
+            this.m_GameBoard = new Card[i_NumberOfRows, i_NumberOfColumns];
             this.m_Player1Score = 0;
             this.m_Player2Score = 0;
             this.m_NameOfPlayer1 = i_NameOfPlayer1;
             this.m_NameOfPlayer2 = i_NameOfPlayer2;
             this.m_MultiPlayerMode = i_MultiPlayerMode;
+            initCards();
 
         }
 
 
-        private char[,] initLettersBehindCards(int i_NumberOfRows, int i_NumberOfColumns)
+        private void initCards()
         {
 
         }
 
-        public int[] FirstMove(int i_PlayerNumber, int i_Row, char i_Column)
+        public void ExposeCard(int i_PlayerNumber, int i_Row, char i_Column)
         {
 
         }
 
-        public bool Second
+        public void GuessCard(int i_PlayerNumber, int i_Row, char i_Column)
+        {
+
+        }
+
+
+        class Card
+        {
+            private char m_Letter;
+            private bool m_Exposed;
+
+            Card(char i_Letter)
+            {
+                this.m_Letter = i_Letter;
+            }
+
+
+        }
 
 
 
